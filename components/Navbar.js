27b2b063cd65bef7a01b123170812671f08d9c22
Munther Basyouni <58863081+muntherb@users.noreply.github.com>
2021-06-09 16:19:22 +0400

    Naming fixes

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,13 +10,13 @@ const Navbar = () => {
     <ul className={styles.nav}>
       <li>
         <Link href="/" as="/" className={styles.link}>
-        {isMobile ?  <FontAwesomeIcon className={window.location.pathname === '/' ?  styles.iconS : styles.icon} icon={faHome}></FontAwesomeIcon> :<a><FontAwesomeIcon className={styles.icon} icon={faHome}></FontAwesomeIcon>Landing page </a> }
+        {isMobile ?  <FontAwesomeIcon className={window.location.pathname === '/' ?  styles.iconS : styles.icon} icon={faHome}></FontAwesomeIcon> :<a><FontAwesomeIcon className={styles.icon} icon={faHome}></FontAwesomeIcon>Home </a> }
         </Link>
       </li>
       <li>
         <Link href="/favorites" as="/favorites">
           
-          {isMobile ?  <FontAwesomeIcon className={window.location.pathname === '/favorites' ?  styles.iconS : styles.icon} icon={faHeart}></FontAwesomeIcon> : <a><FontAwesomeIcon className={styles.icon} icon={faHeart}></FontAwesomeIcon>Favorites</a>}
+          {isMobile ?  <FontAwesomeIcon className={window.location.pathname === '/favorites' ?  styles.iconS : styles.icon} icon={faHeart}></FontAwesomeIcon> : <a><FontAwesomeIcon className={styles.icon} icon={faHeart}></FontAwesomeIcon>Liked</a>}
         </Link>
       </li>
     </ul>
